e65d6f92440aedc2ad37f8b4f7efa497169c382d
Okoli Akachukwu <okoli555aka@gmail.com>
2022-09-27 17:28:29 +0100

    the search bar and data dispaly bar are responsive

--- src/components/SearchHook.js
+++ src/components/SearchHook.js
@@ -1,45 +1,55 @@
-import React from 'react';
-// import background from '../Images/pattern-bg.png';
+import React, { useState } from 'react';
 import DataHook from './DataHook';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function SearchHook() {
-  const sampleInfo = [
-    {
-      id: 1,
-      title: "IP Address",
-      value: "192.212.174.101"
-    },
-    {
-      id: 2,
-      title: "Location",
-      value: "Brooklyn, NY 10001"
-      // value: "MTN NIGERIA Communication limited"
-    },
-    {
-      id: 3,
-      title: "Time Zone",
-      value: "UTC -05:00"
-    },
-    {
-      id: 4,
-      title: "ISP",
-      value: "SpaceX Starlink"
-      // value: "MTN NIGERIA Communication limited"
+  const [info, updatedInfo] = useState(sampleInfo);
+
+  const UpdateInfo = (e) => {
+    e.preventDefault();
+    const newInfo = {
+      id: uuidv4(),
+      title: "default",
+      value: "default"
     }
-  ]
+
+    updatedInfo([...info, newInfo]);
+    console.log(info)
+  }
 
   return (
     <div className='top-part'>
-      {/* <img src={background} alt="illustration of a greater than sign"></img> */}
       <div className='first-part'>
         <h1 className='title'>IP Address Tracker</h1>
         <form id='form'>
           <input type={"text"} id='text' placeholder='Search for any IP address or domain'/>
-          <input type={"submit"} id='submit'/>
+          <input type={"submit"} id='submit' onClick={UpdateInfo}/>
         </form>
         <DataHook info={sampleInfo}/>
       </div>
-
     </div>
   )
 }
+
+const sampleInfo = [
+  {
+    id: 1,
+    title: "IP Address",
+    value: "192.212.174.101"
+  },
+  {
+    id: 2,
+    title: "Location",
+    value: "Brooklyn, NY 10001"
+  },
+  {
+    id: 3,
+    title: "Time Zone",
+    value: "UTC -05:00"
+  },
+  {
+    id: 4,
+    title: "ISP",
+    value: "SpaceX Starlink"
+  }
+]
